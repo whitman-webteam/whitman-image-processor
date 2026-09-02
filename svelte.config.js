32acbcd2ceedb9f98b148ef0d51079cb404bb41a
Josh Hutchinson <hutchinsonj@whitman.edu>
2026-09-02 14:19:55 -0700

    svelte.config.js - use repository name as the base path in production

--- svelte.config.js
+++ svelte.config.js
@@ -2,19 +2,23 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
+		compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html', // Important for routing on GitHub Pages
-			precompress: false,
-			strict: true
-		})
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',// Important for routing on GitHub Pages
+      precompress: false,
+      strict: true
+    }),
+    // Tell SvelteKit to use your repository name as the base path in production
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/whitman-image-processor'
+    }
+  }
 };
 
 export default config;
